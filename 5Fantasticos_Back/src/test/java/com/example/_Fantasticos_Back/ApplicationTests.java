19d6919f0112fcbd9e308e3bb7c5de3f97d8846f
Karol2905 <karol.estupinan-v@mail.escuelaing.edu.co>
2025-09-16 22:56:36 -0500

    inicializacion de Spring

---
+++ 5Fantasticos_Back/src/test/java/com/example/_Fantasticos_Back/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._Fantasticos_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
