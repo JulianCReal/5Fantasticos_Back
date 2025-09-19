b439109a229b7ea6d0115b4ba1fc6fc8d437b744
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-09-18 22:23:58 -0500

    fix - Se borraron las clases SolicitudCambioGrupo.java y SolicitudCambioMateria.java debido a que generaban errores porque ya no se usa herencia

--- 5Fantasticos_Back/src/main/java/com/example/fantasticosback/util/SolicitudCambioMateria.java
+++
@@ -1,9 +0,0 @@
-package com.example.fantasticosback.util;
-
-import java.util.Date;
-public class SolicitudCambioMateria  extends Solicitud {
-    private Materia materiaCambio;
-    public SolicitudCambioMateria(int id, int grupoMateria, String observaciones, String estado, Date fechaSolicitud) {
-        super(id, grupoMateria, observaciones, estado, fechaSolicitud);
-    }
-}
