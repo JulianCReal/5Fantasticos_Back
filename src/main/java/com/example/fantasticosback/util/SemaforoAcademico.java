3f6960fbca0734526320ea1311af73af5a29f5bc
Santiago <santiago.carmona-p@mail.escuelaing.edu.co>
2025-09-24 21:03:56 -0500

    feat - Se añadió código para la clase SemaforoAcademico

--- src/main/java/com/example/fantasticosback/util/SemaforoAcademico.java
+++ src/main/java/com/example/fantasticosback/util/SemaforoAcademico.java
@@ -1,6 +1,58 @@
 package com.example.fantasticosback.util;
 
+import java.util.ArrayList;
+
 public class SemaforoAcademico {
+    private int id;
+    private int avancePorcentaje;
+    private ArrayList<Inscripcion> materias = new ArrayList<>();
+    private double promedioAcumulado; // Cambiar a double para mayor precisión
+    private int totalCreditos;
+    private int creditosAprobados;
+
+    public SemaforoAcademico(int id, int avancePorcentaje, int totalCreditos) {
+        this.id = id;
+        this.avancePorcentaje = avancePorcentaje;
+        this.promedioAcumulado = 0.0; // Cambiar a 0.0
+        this.totalCreditos = totalCreditos;
+        this.creditosAprobados = 0;
+    }
+
+    // METODO INCOMPLETO
+    public void actualizarAvance(ArrayList<Inscripcion> materias) {
+        this.materias = materias;
+
+        for (Inscripcion inscripcion : materias) {
+            if (inscripcion.getEstado().equals("aprobada")) {
+                creditosAprobados += inscripcion.getGrupo().getMateria().getCreditos();
+            }
+        }
+
+        calcularPromedioAcumulado();
+        this.avancePorcentaje = (int) ((double) creditosAprobados / totalCreditos * 100);
+    }
+
+
+    // METODO INCOMPLETO
+    private void calcularPromedioAcumulado() {
+        double sumaNotasCreditos = 0.0;
+
+        for (Inscripcion inscripcion : materias) {
+            if (inscripcion.getEstado().equals("aprobada")) {
+                int creditos = inscripcion.getGrupo().getMateria().getCreditos();
+                double nota = inscripcion.getNotaFinal();
+
+                sumaNotasCreditos += nota * creditos;
+                creditosAprobados += creditos;
+            }
+        }
+            this.promedioAcumulado = sumaNotasCreditos / creditosAprobados;
+    }
+
+    public int getId() {
+        return id;
+    }
+
 
 
 }
