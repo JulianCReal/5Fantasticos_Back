d1b7c3ace3f5604ee70303d432dd2cef7423df57
Sergio Idarraga <chechotorres498@gmail.com>
2025-10-09 21:12:43 -0500

    fix - Se agregó etiqueta @Hidden para evitar conflictos con swagger

--- src/main/java/com/example/fantasticosback/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/fantasticosback/Exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.example.fantasticosback.Exception;
 import com.example.fantasticosback.Dtos.ResponseDTO;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Hidden
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
